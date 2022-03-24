bbc4b9b1e7659abe1b03c8e98617467876ea94bd
Randomystick <bryankhleow@gmail.com>
2022-03-24 09:42:58 +0800

    refactor into new swiper module (ion slides will be deprecated) pt 1

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,6 +6,7 @@ import { HomePage } from './home.page';
 import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { SwiperModule } from 'swiper/angular'
 
 @NgModule({
   imports: [
@@ -13,7 +14,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    SwiperModule
   ],
   declarations: [HomePage]
 })
