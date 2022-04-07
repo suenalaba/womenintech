02b7a3618382266a9a229d1cb666b2887dbd013d
Gladys Loh <gladys.loh@live.com>
2022-04-07 22:47:07 +0800

    minor edits

--- src/app/pages/gym-buddy/gb-findbuddy/gb-findbuddy.page.ts
+++ src/app/pages/gym-buddy/gb-findbuddy/gb-findbuddy.page.ts
@@ -70,14 +70,6 @@ export class GbFindbuddyPage implements OnInit {
 
 
   async matchBuddy() {
-<<<<<<< HEAD
-=======
-    this.findBuddyQuery.addMatches(this.recommendedUser.getUserId);
-    console.log(this.recommendedUser.checkMatches(this.currentUser.getUserId))
-    if(this.recommendedUser.checkMatches(this.currentUser.getUserId)){
-          this.createChat(this.recommendedUser.getUserId,this.currentUser.getUserId);
-    }
->>>>>>> 9a10ba5
     this.recommendedUser=this.recommendationEngine.pollMatch();
     if(!this.recommendedUser){
       this.displayNoMoreMatches();
