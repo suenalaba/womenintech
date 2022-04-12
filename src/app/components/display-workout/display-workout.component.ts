7738a6887b276a3eb19ec62eae8e6774295e9e12
gladysloh <gladys.loh@live.com>
2022-04-13 02:06:26 +0800

    edit workout bug

--- src/app/components/display-workout/display-workout.component.ts
+++ src/app/components/display-workout/display-workout.component.ts
@@ -130,6 +130,8 @@ export class DisplayWorkoutComponent implements OnInit {
 
     this.workoutRoutine[this.exerciseIndex].sets.sets = this.currentExercise.sets.sets+1;
     this.workSets.push(this.currentExercise.sets.reps)
+
+    this.workoutDetails.workoutRoutine = this.workoutRoutine
     window.localStorage.setItem("workoutRoutine", JSON.stringify(this.workoutRoutine));
   }
 
@@ -137,6 +139,7 @@ export class DisplayWorkoutComponent implements OnInit {
     if(this.workSets.length > 0){
       this.workSets.splice(this.selectedSet,1)
       this.workoutRoutine[this.exerciseIndex].sets.sets = this.currentExercise.sets.sets-1;
+      this.workoutDetails.workoutRoutine = this.workoutRoutine
       window.localStorage.setItem("workoutRoutine", JSON.stringify(this.workoutRoutine));
     }
     
