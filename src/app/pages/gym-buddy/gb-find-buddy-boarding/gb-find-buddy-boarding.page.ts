5db8ee82f3d5b55e73e87735e754c1a21af57002
Joshua <joshuajr13@gmail.com>
2022-04-14 02:43:16 +0800

    Update gb-find-buddy-boarding.page.ts to include jsdoc

--- src/app/pages/gym-buddy/gb-find-buddy-boarding/gb-find-buddy-boarding.page.ts
+++ src/app/pages/gym-buddy/gb-find-buddy-boarding/gb-find-buddy-boarding.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { LoadingController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { DbRetrieveService } from 'src/app/services/db-retrieve.service';
 import { GymBuddyProfileInfo } from '../gb-findbuddy/GymBuddyInformation';
@@ -11,68 +10,32 @@ import { GymBuddyService } from 'src/app/services/gym-buddy.service';
   templateUrl: './gb-find-buddy-boarding.page.html',
   styleUrls: ['./gb-find-buddy-boarding.page.scss'],
 })
+/**
+ * The class is used to initialize the display and contents of the find buddy boarding page and loads user information in the background.
+ */
 export class GbFindBuddyBoardingPage implements OnInit {
 
-  people = [
-    {
-      id: '0',
-      name: 'Bryan',
-      briefIntro: 'Loves a hustle',
-      goals: ['Gain muscle mass', 'Lose Weight'],
-      strengths: ['Powerlifting', 'Aerobic'],
-      style: ['high intensity', 'weights'],
-      location: ['outdoor', 'gym'],
-      time: ['Morning', 'Night'],
-      age: 23,
-      img:
-        'https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      //power: 0,
-    },
-    {
-      id: '1',
-      name: 'Gladys',
-      briefIntro: 'Transient and temporary',
-      goals: ['Increase Stamina', 'Lift heavier'],
-      strengths: ['Ploymetric', 'Pilates'],
-      style: ['Flexibility', 'Yoga'],
-      location: ['Studio', 'Park'],
-      time: ['Afternoon', 'Evening'],
-      age: 23,
-      img:
-        'https://images.pexels.com/photos/33045/lion-wild-africa-african.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      //power: 0,
-    },
-    {
-      id: '2',
-      name: 'Zhi Kai',
-      briefIntro: 'Fearless and adventurous',
-      goals: ['Gain muscle mass', 'Lose Weight'],
-      strengths: ['Powerlifting', 'Aerobic'],
-      style: ['high intensity', 'weights'],
-      location: ['outdoor', 'gym'],
-      time: ['Morning', 'Night'],
-      age: 23,
-      img:
-        'https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      //power: 0,
-    },
-  ];
-
-  potentialMatches: (GymBuddyProfileInfo)[] = [];
+  /**
+   * An array of gym buddy profiles suggested by the matching algorithm.
+   */
+  public potentialMatches: (GymBuddyProfileInfo)[] = [];
 
 
   private recommendationEngine;
   private currentUser: GymBuddyProfileInfo;
-  private recommendedUser: GymBuddyProfileInfo;
   private findBuddyQuery: FindBuddyQuery;
 
   constructor(
-    private loadingController: LoadingController,
     private router: Router,
     private dbRetrieve: DbRetrieveService,
     private gbService: GymBuddyService
   ) { }
 
+  /**
+   * Main entry point into the page that runs when the class is instantiated.
+   * This method will retrieve user details from the database and instantiates the Recommendation Engine to source for potential matches.
+   * The potential matches will then be stored in an array of Gym Buddy Profile objects.
+   */
   async ngOnInit() {
     this.currentUser = this.dbRetrieve.retrieveCurrentUser();
     this.recommendationEngine = new RecommendationEngine(this.currentUser);
@@ -91,7 +54,6 @@ export class GbFindBuddyBoardingPage implements OnInit {
         //the front of array, so it will be displayed last.
         this.potentialMatches.unshift(potentialMatch);
       } else {
-        console.log('instant chao');
         break;
       }
     }
@@ -99,7 +61,8 @@ export class GbFindBuddyBoardingPage implements OnInit {
   }
 
   /**
-   * Continue Navigate to find a buddy.
+   * This method is called when user selects the button to navigate to find a buddy page.
+   * This will route the page to the next page.
    */
    public async goToFindBuddy() {
     this.router.navigateByUrl('tabs/gym-buddy/gb-findbuddy', { replaceUrl: true });
