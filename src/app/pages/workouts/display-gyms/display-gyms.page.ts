ced41ce2dce398092673ff6a7cf0e5b295ff19a4
Gabriel <gaabriel.tang@gmail.com>
2022-04-14 13:13:31 +0800

    update

--- src/app/pages/workouts/display-gyms/display-gyms.page.ts
+++ src/app/pages/workouts/display-gyms/display-gyms.page.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewChild, ElementRef } from '@angular/core';
 
-declare var google: any;
+declare let google: any;
 
 @Component({
   selector: 'app-display-gyms',
   templateUrl: './display-gyms.page.html',
   styleUrls: ['./display-gyms.page.scss'],
 })
+
+
 export class DisplayGymsPage implements OnInit {
 
   map: any;
@@ -17,54 +19,54 @@ export class DisplayGymsPage implements OnInit {
   infoWindows: any = [];
   markers: any = [
     {
-        title: "Anytime Fitness Nanyang CC",
-        latitude: "1.3446769843267867",
-        longitude: "103.6921381067168"
+        title: 'Anytime Fitness Nanyang CC',
+        latitude: '1.3446769843267867',
+        longitude: '103.6921381067168'
     },
     {
-        title: "Jurong West ActiveSG Gym",
-        latitude: "1.3403339174867968",
-        longitude: "103.69367161695456"
+        title: 'Jurong West ActiveSG Gym',
+        latitude: '1.3403339174867968',
+        longitude: '103.69367161695456'
     },
     {
-        title: "Fitness First - One George Street",
-        latitude: "1.28621796655656",
-        longitude: "103.8475568003505"
+        title: 'Fitness First - One George Street',
+        latitude: '1.28621796655656',
+        longitude: '103.8475568003505'
     },
     {
-        title: "NTU Sports and Recreation Centre (SRC) Hall D",
-        latitude: "1.349639123551359",
-        longitude: "103.68782849842782"
+        title: 'NTU Sports and Recreation Centre (SRC) Hall D',
+        latitude: '1.349639123551359',
+        longitude: '103.68782849842782'
     },
     {
-        title: "NTU Staff Club Gym",
-        latitude: "1.3511795141546894",
-        longitude: "103.68801192920789"
+        title: 'NTU Staff Club Gym',
+        latitude: '1.3511795141546894',
+        longitude: '103.68801192920789'
     },
     {
-      title: "The Wave",
-      latitude: "1.349309039723673",
-      longitude: "103.68944268929248"
+      title: 'The Wave',
+      latitude: '1.349309039723673',
+      longitude: '103.68944268929248'
     },
     {
-      title: "North Hill Gym, Binjai Hall 19A",
-      latitude: "1.3548348111666786",
-      longitude: "103.68787552913615"
+      title: 'North Hill Gym, Binjai Hall 19A',
+      latitude: '1.3548348111666786',
+      longitude: '103.68787552913615'
     },
     {
-      title: "Spartans Boxing Club Jurong West",
-      latitude: "1.3503332109014345",
-      longitude: "103.70296136890298"
+      title: 'Spartans Boxing Club Jurong West',
+      latitude: '1.3503332109014345',
+      longitude: '103.70296136890298'
     },
     {
-      title: "Amore Fitness & Define",
-      latitude: "1.3001766947873528",
-      longitude: "103.84537272919611"
+      title: 'Amore Fitness & Define',
+      latitude: '1.3001766947873528',
+      longitude: '103.84537272919611'
     },
     {
-      title: "Vigeo Fitness | Personal Training Singapore",
-      latitude: "1.2978813139944605",
-      longitude: "103.8449650334324"
+      title: 'Vigeo Fitness | Personal Training Singapore',
+      latitude: '1.2978813139944605',
+      longitude: '103.8449650334324'
     }
   ];
 
@@ -74,12 +76,15 @@ export class DisplayGymsPage implements OnInit {
     this.showMap();
   }
 
-  
+/**
+ *
+ * @param markers
+ */
   addMarkersToMap(markers) {
-    for (let marker of markers) {
-      let position = new google.maps.LatLng(marker.latitude, marker.longitude);
-      let mapMarker = new google.maps.Marker({
-        position: position,
+    for (const marker of markers) {
+      const position = new google.maps.LatLng(marker.latitude, marker.longitude);
+      const mapMarker = new google.maps.Marker({
+        position,
         title: marker.title,
         latitude: marker.latitude,
         longitude: marker.longitude
@@ -89,15 +94,18 @@ export class DisplayGymsPage implements OnInit {
       this.addInfoWindowToMarker(mapMarker);
     }
   }
-
+/**
+ *
+ * @param marker
+ */
   addInfoWindowToMarker(marker) {
-    let infoWindowContent = '<div id="content">' +
+    const infoWindowContent = '<div id="content">' +
                               '<h2 id="firstHeading" class"firstHeading">' + marker.title + '</h2>' +
                               '<p>Latitude: ' + marker.latitude + '</p>' +
                               '<p>Longitude: ' + marker.longitude + '</p>' +
                             '</div>';
 
-    let infoWindow = new google.maps.InfoWindow({
+    const infoWindow = new google.maps.InfoWindow({
       content: infoWindowContent
     });
 
@@ -109,7 +117,7 @@ export class DisplayGymsPage implements OnInit {
   }
 
   closeAllInfoWindows() {
-    for(let window of this.infoWindows) {
+    for(const window of this.infoWindows) {
       window.close();
     }
   }
