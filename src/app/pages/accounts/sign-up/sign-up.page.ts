0545c16aa3f1916bf1087a8337ef5958f37258bd
gladysloh <gladys.loh@live.com>
2022-04-14 11:50:34 +0800

    java docs - sign up

--- src/app/pages/accounts/sign-up/sign-up.page.ts
+++ src/app/pages/accounts/sign-up/sign-up.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
-import {  Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 import { NavController, AlertController, LoadingController, Platform } from '@ionic/angular';
 import { AuthenticationService } from './../../../services/authentication.service';
@@ -36,109 +36,125 @@ export class SignUpPage implements OnInit {
     });
   }
 
-    // Easy access for form fields
-    get firstName() {
-      return this.credentials.get('firstName');
+  // Easy access for form fields
+  get firstName() {
+    return this.credentials.get('firstName');
+  }
+
+  get lastName() {
+    return this.credentials.get('lastName');
+  }
+
+  get confirmPassword() {
+    return this.credentials.get('confirmPassword');
+  }
+
+  get password() {
+    return this.credentials.get('password');
+  }
+
+  get username() {
+    return this.credentials.get('username');
+  }
+
+  get email() {
+    return this.credentials.get('email');
+  }
+
+  get gender() {
+    return this.credentials.get('gender');
+  }
+
+  get birthday() {
+    return this.credentials.get('birthday');
+  }
+
+  /**
+   * checks if confirm password matches with password, it will return validation error is it does not match,
+   * else it will reutrn null as there is not error
+   *
+   * @param password user password 
+   */
+  equalto(field_name): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+
+      let input = control.value;
+      let isValid = control.root.value[field_name] == input
+      if (!isValid)
+        return { 'equalTo': { isValid } }
+      else
+        return null;
+    };
+  }
+
+  /**
+   * Navigate to login page
+   */
+  async login() {
+    this.router.navigateByUrl('', { replaceUrl: true });
+  }
+
+  /**
+   * funcion to register a new user 
+   * 
+   * if the user is new and has not made an account, they will be navigated to enter more details for their profile
+   * if the user has registerd before, they will be prompted a failed message 
+   *  
+   */
+  async signUp() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+
+    const user = await this.authService.register(this.credentials.value);
+    await loading.dismiss();
+
+
+    if (this.credentials.valid && user) {
+      this.credentials.value.id = user.user.uid;
+      localStorage.setItem('userSignUp', JSON.stringify(this.credentials.value));
+      this.router.navigateByUrl('/user-details', { replaceUrl: true });
+    } else {
+      this.showAlert('Registration failed', 'Please try again!');
     }
 
-    get lastName() {
-      return this.credentials.get('lastName');
+  }
+
+  /**
+   * function to display alert with customized headers and messages
+   * 
+   * @param header header of alert
+   * @param message message of alert
+   */
+  async showAlert(header, message) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  /**
+   * function to display loading component
+   */
+  async showLoading() {
+    this.loadingPresent = true
+    let load = await this.loadingController.create({
+      message: "Please wait....",
+
+    })
+    await load.present();
+  }
+
+  /**
+   * fucntion to dismiss loading component
+   */
+  async dismissLoading() {
+    if (this.loadingPresent) {
+      await this.loadingController.dismiss();
     }
-
-    get confirmPassword() {
-      return this.credentials.get('confirmPassword');
-    }
-
-    get password() {
-      return this.credentials.get('password');
-    }
-
-    get username() {
-      return this.credentials.get('username');
-    }
-
-    get email() {
-      return this.credentials.get('email');
-    }
-
-    get gender() {
-      return this.credentials.get('gender');
-    }
-
-    get birthday() {
-      return this.credentials.get('birthday');
-    }
-
-    /**
-     *
-     * @param password
-     */
-    equalto(field_name): ValidatorFn {
-      return (control: AbstractControl): { [key: string]: any } => {
-
-        let input = control.value;
-        let isValid = control.root.value[field_name] == input
-        if (!isValid)
-          return { 'equalTo': { isValid } }
-        else
-          return null;
-      };
-    }
-
-    /**
-     * Navigate to login page
-     */
-    async login() {
-      this.router.navigateByUrl('', { replaceUrl: true });
-    }
-
-    /**
-     *
-     * sign
-     */
-    async signUp() {
-      const loading = await this.loadingController.create();
-      await loading.present();
-
-
-      const user = await this.authService.register(this.credentials.value);
-      await loading.dismiss();
-      
-
-      if (this.credentials.valid && user) {
-        this.credentials.value.id = user.user.uid;
-        localStorage.setItem('userSignUp', JSON.stringify(this.credentials.value));
-        this.router.navigateByUrl('/user-details', { replaceUrl: true });
-        // this.router.navigateByUrl('/boarding', { replaceUrl: true });
-      } else {
-        this.showAlert('Registration failed', 'Please try again!');
-      }
-
-    }
-
-    async showAlert(header, message) {
-      const alert = await this.alertController.create({
-        header,
-        message,
-        buttons: ['OK'],
-      });
-      await alert.present();
-    }
-
-    async showLoading() {
-      this.loadingPresent = true
-      let load = await this.loadingController.create({
-        message: "Please wait....",
-
-      })
-      await load.present();
-    }
-
-    async dismissLoading() {
-      if (this.loadingPresent) {
-        await this.loadingController.dismiss();
-      }
-      this.loadingPresent = false;
-    }
+    this.loadingPresent = false;
+  }
 
 }
