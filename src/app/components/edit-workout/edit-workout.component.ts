bdde4ace25b8605b185fcc6760d93d00fac6da3c
gladysloh <gladys.loh@live.com>
2022-04-14 11:40:50 +0800

    java docs - edit workout component

--- src/app/components/edit-workout/edit-workout.component.ts
+++ src/app/components/edit-workout/edit-workout.component.ts
@@ -2,8 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 import { WorkoutDesc } from 'src/app/class/CreateWorkoutDesc';
-import { WorkoutDetails } from 'src/app/class/WorkoutDetails';
-import { WorkoutsService } from 'src/app/services/workouts/workouts.service';
 
 @Component({
   selector: 'app-edit-workout',
@@ -11,6 +9,7 @@ import { WorkoutsService } from 'src/app/services/workouts/workouts.service';
   styleUrls: ['./edit-workout.component.scss'],
 })
 export class EditWorkoutComponent implements OnInit {
+
   @Input() workoutDetails: WorkoutDesc
   @Input() exerciseIndex: any;
   @Input() sets: any;
@@ -21,7 +20,8 @@ export class EditWorkoutComponent implements OnInit {
 
   set: any;
   rep: any;
-  constructor(private fb: FormBuilder, private workoutService: WorkoutsService, private modalController: ModalController) { }
+
+  constructor(private fb: FormBuilder, private modalController: ModalController) { }
 
   ngOnInit() {
     this.userWorkoutUpdates = this.fb.group({
@@ -33,33 +33,38 @@ export class EditWorkoutComponent implements OnInit {
     this.rep = parseInt(this.sets.reps)
   }
 
+  /**
+   * get method for sets
+   * @param s sets
+   */
   getSets(s: number){
     this.set = s
   }
 
+  /**
+   * get method for reps
+   * @param r reps
+   */
   getReps(r: number){
     this.rep = r
   }
 
+  /**
+   * update user sets and reps
+   */
    updateWorkoutDetails() {
     console.log(this.userWorkoutUpdates.controls['sets'].value)
-    // console.log(this.userUpdates)
     if (this.userWorkoutUpdates) {
-      /*update into firestore */
       console.log(this.workoutDetails.workoutRoutine[this.exerciseIndex])
       this.workoutDetails.workoutRoutine[this.exerciseIndex].sets = {
         sets: parseInt(this.set),
         reps: parseInt(this.rep)
       }
-
-      // this.workoutService.saveWorkout(this.wid,this.uid,this.workoutDetails);
     }
 
     this.modalController.dismiss({
       'dismissed': true
     });
-    
-    // await this.saveWorkout(wid, uid, userWorkout)
   }
 
 }
