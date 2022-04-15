e393cf6450370a19458a9ae314e57fb8ade323de
Joshua <joshuajr13@gmail.com>
2022-04-16 01:41:39 +0800

    Update environment.ts to fix eslint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyCfbIAq8CRsZ1hgrCpShZYLVlNC9GxkfZY",
-    authDomain: "witfit-cca15.firebaseapp.com",
-    projectId: "witfit-cca15",
-    storageBucket: "witfit-cca15.appspot.com",
-    messagingSenderId: "718239768342",
-    appId: "1:718239768342:web:91ccf59d3f0fc97954fa37",
-    measurementId: "G-93R18TE6L6"
+    apiKey: 'AIzaSyCfbIAq8CRsZ1hgrCpShZYLVlNC9GxkfZY',
+    authDomain: 'witfit-cca15.firebaseapp.com',
+    projectId: 'witfit-cca15',
+    storageBucket: 'witfit-cca15.appspot.com',
+    messagingSenderId: '718239768342',
+    appId: '1:718239768342:web:91ccf59d3f0fc97954fa37',
+    measurementId: 'G-93R18TE6L6'
   }
 
 };
