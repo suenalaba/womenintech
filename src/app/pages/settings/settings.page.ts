1c02f5659519b3804fe4c6f9c804c1279ccc2b24
Joshua <joshuajr13@gmail.com>
2022-04-16 02:26:28 +0800

    Update settings.page.ts

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../services/authentication.service';
 import { UserService } from '../../services/user.service';
@@ -13,23 +13,22 @@ import { UserService } from '../../services/user.service';
 /**
  * Settings page display
  */
-export class SettingsPage {
+export class SettingsPage implements OnInit {
 
-  private firstName = '';
-  private settingstat= [
+  public settingstat= [
     {stat: 'GENDER'},
     {stat: 'UNITS OF MEASURE'},
     {stat: 'DATE OF BIRTH'}
   ];
-  private settingvalue = ['MALE','KG','BIRTHDAY'];
-  private userInfo: any;
+  public settingvalue = ['MALE','KG','BIRTHDAY'];
+  public userInfo: any;
   constructor(
     //private avatarService: AvatarService,
     private authService: AuthenticationService,
     private userService: UserService,
     private router: Router  ) {}
 
-  private ngOnInit() {
+  ngOnInit() {
     this.userService.getUserById(JSON.parse(localStorage.getItem('userID'))).subscribe((res)=>{
       this.userInfo = res;
     });
